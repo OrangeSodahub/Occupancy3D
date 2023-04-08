5ac7c3bc8f3ead03a214e9fc1fa6fb119d7a65eb
YangXiuyu <gzzyyxy@gmail.com>
2023-04-08 13:44:10 +0800

    Revert "stash"
    This reverts commit 0fa0c9614a13df75cf94665a0975a3a3b5a6e960.

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -8,4 +8,4 @@ NCCL_DEBUG=INFO
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train_stage_1.py $CONFIG --work-dir ${SAVE_PATH}  --launcher pytorch ${@:4} --deterministic
+    $(dirname "$0")/train.py $CONFIG --work-dir ${SAVE_PATH}  --launcher pytorch ${@:4} --deterministic
