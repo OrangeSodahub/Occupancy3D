be6a6fddaa8f9d4dc8204d2bce830cc89155157d
YangXiuyu <gzzyyxy@gmail.com>
2023-05-19 19:44:06 +0800

    cleanup

--- projects/mmdet3d_plugin/surroundocc/modules/__init__.py
+++ projects/mmdet3d_plugin/surroundocc/modules/__init__.py
@@ -1,6 +1,5 @@
 from .transformer import PerceptionTransformer
 from .spatial_cross_attention import SpatialCrossAttention, MSDeformableAttention3D
 from .encoder import OccEncoder, OccLayer
-from .base_depth_net import BaseDepthNet
 from .image_view_transformer import LSSViewTransformerBEVStereo
 from .occ_fuser import OccFuser
